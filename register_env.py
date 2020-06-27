560d1a9fa11c6256e6212a23885fe20d2f9d525b
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-06-26 23:03:17 -0400

    fix action space in register_env.py

--- register_env.py
+++ register_env.py
@@ -8,28 +8,28 @@ class MineRLObservationWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
         self.observation_space = gym.spaces.Tuple((self.observation_space['pov'], self.observation_space['vector']))
-    
+
     def observation(self, observation):
         return observation['pov'], observation['vector']
 
-    
+
 class MineRLActionWrapper(gym.ActionWrapper):
     def __init__(self, env):
         super().__init__(env)
         self.action_space = self.action_space['vector']
-    
+
     def action(self, action):
         return dict(vector=action)
 
     def reverse_action(self, action):
         return action['vector']
-    
-    
+
+
 for env_spec in minerl.herobraine.envs.ENVS:
     def env_creator(env_config):
-        kwargs=dict(
+        kwargs = dict(
             observation_space=env_spec.observation_space,
-            action_space=env_spec.observation_space,
+            action_space=env_spec.action_space,
             docstr=env_spec.get_docstring(),
             xml=os.path.join(minerl.herobraine.env_spec.MISSIONS_DIR, env_spec.xml),
             env_spec=env_spec,
@@ -37,4 +37,6 @@ for env_spec in minerl.herobraine.envs.ENVS:
         env = minerl.env.MineRLEnv(**kwargs)
         env = MineRLActionWrapper(MineRLObservationWrapper(env))
         return env
+
+
     register_env(env_spec.name, env_creator)
