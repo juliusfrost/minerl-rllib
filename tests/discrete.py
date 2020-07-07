d0b1f4325cb18380517216be55a9b884829e1b27
Julius Frost <juliustfrost@gmail.com>
2020-07-06 21:20:48 -0400

    discrete test

---
+++ tests/discrete.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import gym
+from tqdm import tqdm
+import minerl
+from register_env import wrap
+
+
+def test_discrete():
+    os.chdir('..')
+    env = gym.make('MineRLTreechopVectorObf-v0')
+    env = wrap(env, discrete=True, num_actions=32)
+
+    for _ in tqdm(range(100)):
+        print(env.reverse_action(np.random.randn(1, 64)))
+
+    done = False
+    obs = env.reset()
+    for _ in tqdm(range(100)):
+        if done:
+            break
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+
+
+if __name__ == '__main__':
+    test_discrete()
