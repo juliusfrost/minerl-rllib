1195e2e236f761c97b66a1a342f2416adbe985e9
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-07-10 13:58:40 -0400

    Add competition limits

--- train.py
+++ train.py
@@ -193,6 +193,13 @@ def run(args, parser):
                 input_evaluation=['simulation'],
             ))
 
+        if 'time_total_s' not in args.stop:
+            # The MineRL competition training time limit is 4 days. Subtract an hour for evaluation.
+            args.stop['time_total_s'] = int(4 * 24 * 60 * 60 - 3600)
+        if 'num_steps_sampled' not in args.stop:
+            # The MineRL competition environment sample limit is 8 million steps.
+            args.stop['num_steps_sampled'] = 8000000
+
         # Note: keep this in sync with tune/config_parser.py
         args_experiments = {
             experiment_name: {  # i.e. log to ~/ray_results/default
