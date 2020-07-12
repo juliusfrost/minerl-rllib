8a2bdd1eb2911dd69bc576aa10d6886560004989
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-07-11 21:03:17 -0400

    sum rewards in action repeat wrapper

--- env.py
+++ env.py
@@ -69,11 +69,13 @@ class MineRLActionRepeat(gym.Wrapper):
 
     def step(self, action):
         obs, reward, done, info = None, None, None, None
+        total_reward = 0
         for _ in range(self.action_repeat):
             obs, reward, done, info = self.env.step(action)
+            total_reward += reward
             if done:
                 break
-        return obs, reward, done, info
+        return obs, total_reward, done, info
 
 
 class MineRLObservationStack(gym.Wrapper):
