f10b19518968615b4c4240d5a645d9c139170f57
Julius <juliustfrost@gmail.com>
2020-09-25 01:53:21 -0400

    check for converted data as specified by config and generate it if necessary

--- generate_kmeans.py
+++ generate_kmeans.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--env', default=None)
 parser.add_argument('--num-actions', type=int, default=32)
 parser.add_argument('--data-dir', default=os.getenv('MINERL_DATA_ROOT', 'data'))
+parser.add_argument('--overwrite', action='store_true')
 
 
 def main():
@@ -25,6 +26,15 @@ def main():
 
     for env_name in env_list:
         print(f'Generating {args.num_actions}-means for {env_name}')
+
+        file_dir = os.path.join(args.data_dir, f'{args.num_actions}-means')
+        file = os.path.join(file_dir, env_name + '.npy')
+        if os.path.exists(file) and not args.overwrite:
+            print(f'k-means file already exists at {file}')
+            continue
+        if not os.path.exists(file_dir):
+            os.mkdir(file_dir)
+
         data = minerl.data.make(env_name)
         actions = []
         for trajectory_name in tqdm(list(data.get_trajectory_names())):
@@ -37,10 +47,6 @@ def main():
         print('computing k-means...')
         kmeans = KMeans(n_clusters=args.num_actions, verbose=1, random_state=0).fit(actions)
         print(kmeans)
-        file_dir = os.path.join(args.data_dir, f'{args.num_actions}-means')
-        if not os.path.exists(file_dir):
-            os.mkdir(file_dir)
-        file = os.path.join(file_dir, env_name + '.npy')
         np.save(file, kmeans.cluster_centers_)
 
 
